e597c64ad6722c05ecc5ac58055984843346622a
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 19:51:13 +0600

    A Python program that demonstrates the use of sets.

---
+++ Set.py
@@ -0,0 +1,12 @@
+# Set.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates the use of sets.
+
+# Creating a set of marks
+marks = {85, 90, 90, 92, 88} 
+print("Marks Set:", marks) 
+# Duplicates will be removed
+# No indexing in sets (Unordered)
+
+for score in marks:
+    print(score)
