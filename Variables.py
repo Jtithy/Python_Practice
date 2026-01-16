a22e3e9111cd3e5c255958eff12ce3dfc90cf691
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 12:31:44 +0600

    Add Variables.py: A Python program that demonstrates the use of variables

---
+++ Variables.py
@@ -0,0 +1,14 @@
+# Variables.py
+#Date: 2024-06-15
+#Description: A Python program that demonstrates the use of variables.
+
+name1 = "Tithy"
+age1 = 22
+
+name2 = "Tahmid"
+age2 = 20.5
+is_adult = True
+print(name1)
+print(age1)
+print(name2)
+print(age2)
