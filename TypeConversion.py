4271b73b54c9d8148fe171ffa4946d91c17f2758
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 16:55:19 +0600

    A Python program that demonstrates type conversion.

---
+++ TypeConversion.py
@@ -0,0 +1,8 @@
+# TypeConversion.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates type conversion.
+
+oldAge = input("Please enter your old age: ")
+# int(oldAge) :Converting string input to integer
+newAge = int(oldAge) + 2
+print(newAge)
