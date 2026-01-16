a1e574fae703d9d08f71f654fa22fa8103538475
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 16:49:19 +0600

    Add AskName.py: A Python program that asks for the user's name and greets them

---
+++ AskName.py
@@ -0,0 +1,6 @@
+# AskName.py
+# Date: 2024-06-15
+# Description: A Python program that asks for the user's name and greets them.
+
+name = input("Please enter your name: ")
+print("Assalamualaikum, " + name + "! Have a great day ahead!")
