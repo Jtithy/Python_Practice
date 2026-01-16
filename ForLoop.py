4e859f0fa30407f5cde7ac13c454bba35cfa8ad1
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 18:57:50 +0600

    A Python program that demonstrates a for loop.

---
+++ ForLoop.py
@@ -0,0 +1,13 @@
+# ForLoop.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates a for loop.
+
+# Printing numbers from 0 to 4 using a for loop
+print("For Loop from 0 to 4:")
+for item in range(5): # 0 to 4
+    print(item)
+
+# Printing numbers from 4 to 0 using a for loop
+print("For Loop from 4 to 0:")
+for item in range(4, -1, -1): # 4 to 0
+    print(item)
