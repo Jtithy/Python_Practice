bf02396349410949b3dd37529587a6fabae69c32
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 17:31:37 +0600

    A Python program that demonstrates string manipulation.

---
+++ Strings.py
@@ -0,0 +1,11 @@
+# Strings.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates string manipulation.
+
+name = "Levi Ackermann"
+
+print(name.upper()) # Convert to uppercase
+print(name.lower()) # Convert to lowercase
+print(name.find("Acker")) # Find substring
+print("Acker" in name) # Check substring presence
+print(name.replace("Levi", "Mikasa")) # Replace substring
