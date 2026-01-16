03b765078294e6a5a0af9d032a56892f9fb0ab30
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 18:14:13 +0600

    A Python program that demonstrates operator precedence in expressions.

---
+++ OperatorPrecedence.py
@@ -0,0 +1,6 @@
+# OperatorPrecedence.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates operator precedence in expressions.
+
+result = 10 + 5 * 2 * 2 - (8 / 4)
+print("Result of expression:", result)  # Expected output
