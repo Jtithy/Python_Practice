6e2ead6beb03fab97a2e9a72d0690c1aa580deb9
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 16:40:15 +0600

    Add PersonDetails.py: A Python program that defines and prints person details

---
+++ PersonDetails.py
@@ -0,0 +1,9 @@
+# PersonDetails.py
+# Date: 2024-06-15
+# Description: A Python program that defines and prints person details.
+
+name = "Megumi Fusiguru"
+age = 23
+print(name)
+print(age)
+print(name + ", is a genius.")
