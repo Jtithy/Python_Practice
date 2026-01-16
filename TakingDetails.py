471634e03dacd6aa39b9a1526f10c81311f7a7d8
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 16:46:00 +0600

    Add TakingDetails.py: A Python program that takes input and prints person details

---
+++ TakingDetails.py
@@ -0,0 +1,9 @@
+# TakingDetails.py
+# Date: 2024-06-15
+# Description: A Python program that takes input and prints person details.
+
+name = input("What's your name?")
+age = input("What's your age?")
+print("Your name is: " + name)
+print("Your age is: " + age)
+print(name + ", you're genius!")
