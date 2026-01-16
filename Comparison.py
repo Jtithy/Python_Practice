cd245d4fcd25d1e03333169c52c4b3f8d2f7458f
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 18:19:17 +0600

    A Python program that compares basic arithmetic operations, string manipulations, and operator precedence.

---
+++ Comparison.py
@@ -0,0 +1,16 @@
+# Comparison.py
+# Date: 2024-06-15
+# Description: A Python program that compares basic arithmetic operations, string manipulations, and operator precedence.
+
+print("Comparison of Different Python Concepts")
+#Taking input for comparisons
+num1 = input("Enter 1st number for comparison: ")
+num2 = input("Enter 2nd number for comparison: ")
+
+#Performing comparison operations
+print("Greater Than:", int(num1) > int(num2))  # Greater Than Operator
+print("Less Than:", int(num1) < int(num2))  # Less Than Operator
+print("Greater Than or Equal To:", int(num1) >= int(num2)) # Greater Than or Equal To Operator
+print("Less Than or Equal To:", int(num1) <= int(num2)) # Less Than or Equal To Operator
+print("Not Equal To:", int(num1) != int(num2)) # Not Equal To Operator
+print("Equality:", int(num1) == int(num2)) # Equality Operator
