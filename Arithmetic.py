db59109d1fe90fec0ab90f2abec8320744f2513f
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 17:57:30 +0600

    A Python program that demonstrates basic arithmetic operations.

---
+++ Arithmetic.py
@@ -0,0 +1,13 @@
+# Arithmetic.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates basic arithmetic operations.
+
+value1 = input("Enter 1st number: ")
+value2 = input("Enter 2nd number: ")
+print("Addition: " + str(int(value1) + int(value2))) # Addition
+print("Subtraction: " + str(int(value1) - int(value2))) # Subtraction
+print("Multiplication: " + str(int(value1) * int(value2))) # Multiplication
+print("Floating Division: " + str(int(value1) / int(value2))) # Division
+print("Integer Division: " + str(int(value1) // int(value2))) # Division
+print("Modulus/Remainder: " + str(int(value1) % int(value2))) # Modulus
+print("Exponentiation/Power: " + str(int(value1) ** int(value2))) # Exponentiation
