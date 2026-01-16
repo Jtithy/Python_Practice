1f6a73d947434f22b5a99aab595a662bebfe5489
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 20:27:33 +0600

    A Python program that demonstrates user-defined functions.

---
+++ UserDefinedFunction.py
@@ -0,0 +1,8 @@
+# UserDefinedFunction.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates user-defined functions.
+
+def add_num(a,b):
+    print("Sum is: ", a+b)
+
+add_num(5,10)
