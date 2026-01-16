d2d3e745bbbf344095424ee308f8d1fa353d98e2
jtithy <tithyjebunnesa202@gmail.com>
2026-01-16 18:44:31 +0600

    A Python program that demonstrates the use of the range() function.

---
+++ Range.py
@@ -0,0 +1,6 @@
+# Range.py
+# Date: 2024-06-15
+# Description: A Python program that demonstrates the use of the range() function.
+
+numbers = range(5) # 0 to 5
+print(numbers) # Prints range object
